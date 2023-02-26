c9f0a5835e6639faa44cd0300a02cbb9fcdd8bc9
dongwoochoi <dong99woo@gmail.com>
2023-02-27 00:25:16 +0900

    Context API까지

--- src/page/detail.tsx
+++ src/page/detail.tsx
@@ -1,9 +1,11 @@
 import { Shoesdata } from "../App";
 import { useParams } from 'react-router-dom';
 import {useState, useEffect} from 'react';
+import { Nav } from "react-bootstrap";
 // import styled from 'styled-components';
 
 function Detail({shoes}:{shoes:Shoesdata[]}){
+    let [tab, set_tab] = useState(0)
     let {id} = useParams() as { id: string };
     let searchid = shoes.find(function(x){
         return x.id == parseInt(id)
@@ -23,8 +25,36 @@ function Detail({shoes}:{shoes:Shoesdata[]}){
                         <button className="btn btn-danger">주문하기</button> 
                     </div>
                 </div>
-                </div> 
+                <Nav variant="tabs"  defaultActiveKey="link0">
+                    <Nav.Item>
+                        <Nav.Link onClick={()=>{ set_tab(0) }} eventKey="link0">버튼0</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={()=>{ set_tab(1) }} eventKey="link1">버튼1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link onClick={()=>{ set_tab(2) }} eventKey="link2">버튼2</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+                <TabContent tab={tab}/>
+            </div>
+             
         </div>
     );
 }
 export default Detail;
+
+function TabContent({tab}:{tab:number}){
+    let [fade, setFade] = useState('')
+    useEffect(()=>{
+        setTimeout(()=>{ setFade('end') }, 100)
+    return ()=>{
+        setFade('')
+    }
+    }, [tab])
+    return (
+        <div className={"start"+" "+ fade}>
+          { [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab] }
+        </div>
+      )
+  }
